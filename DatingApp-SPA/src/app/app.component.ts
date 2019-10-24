6aeb00d6f88f03172ee026764aa62639f8a7d99b
Juan Zamora Ramírez <Juan Jose@JUANJOSE-PC.internal.osas.com>
2019-10-23 23:14:32 -0500

    End of section 6

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthService } from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'DatingApp from Juan';
+export class AppComponent implements OnInit {
+  jwtHelper = new JwtHelperService();
+
+  token = localStorage.getItem('token');
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authService.decodedToken = this.jwtHelper.decodeToken(this.token);
+  }
 }
